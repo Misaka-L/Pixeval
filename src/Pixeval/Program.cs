66a39dc804ade188fe3d9d9a3e1f4da3eb7cce2d
Summpot <summpot@outlook.com>
2020-10-02 15:16:02 +0800

    try hosting

---
+++ src/Pixeval/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Pixeval
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+
+                })
+                .Build().RunAsync();
+        }
+    }
+}
