1494f7e73df6ca5a4a3b486800c943be3d384d7c
Summpot <summpot@outlook.com>
2020-10-02 18:56:09 +0800

    add file header

--- src/Pixeval/Core/IQualifier.cs
+++ src/Pixeval/Core/IQualifier.cs
@@ -1,4 +1,11 @@
-﻿#region Copyright (C) 2019-2020 Dylech30th. All rights reserved.
+﻿// Pixeval - A Strong, Fast and Flexible Pixiv Client
+//  Copyright (C) 2019-2020 Dylech30th
+// This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+
+#region Copyright (C) 2019-2020 Dylech30th. All rights reserved.
 
 // Pixeval - A Strong, Fast and Flexible Pixiv Client
 // Copyright (C) 2019-2020 Dylech30th
--- src/Pixeval/Program.cs
+++ src/Pixeval/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Pixeval - A Strong, Fast and Flexible Pixiv Client
+//  Copyright (C) 2019-2020 Dylech30th
+// This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
